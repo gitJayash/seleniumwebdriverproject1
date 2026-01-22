85542bc3ef5e5689ecf50b07658d2601338708fb
gitJayash <jayashsandaruwan@gmail.com>
2026-01-22 19:02:53 +0530

    First commit for day46

---
+++ src/test/java/day46/OrangeHRM.java
@@ -0,0 +1,22 @@
+package day46;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class OrangeHRM {
+
+    WebDriver driver;
+
+    @BeforeClass
+
+    void setUp()
+    {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        driver.manage().window().maximize();
+    }
+}
